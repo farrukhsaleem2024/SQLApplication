fd087d5625cd73cd44ca44b4d6de0968950290e8
farrukhsaleem2024 <akhtarparveen2024@hotmail.com>
2024-03-12 23:40:54 +0500

    Add new Function App

---
+++ FunctionApp1/Function1.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+        private static SqlConnection GetConnection()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_SQLConnectionString");
+            //string connectionString = "Server=tcp:webappdbsrvr.database.windows.net,1433;Initial Catalog=webappdb;Persist Security Info=False;User ID=webappdbsrvr;Password=WebAppDbServer2024;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return new SqlConnection(connectionString);
+
+        }
+        [Function("Function1")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+
+        [Function("GetProducts")]
+        public static async Task<IActionResult> RunProducts(
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+           ILogger log)
+        {
+            SqlConnection conn = GetConnection();
+            List<Product> _products = new List<Product>();
+            string statement = "SELECT ProductID,ProductName,Quantity from Products";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(statement, conn);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ProductID = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+                    };
+                    _products.Add(product);
+                }
+            }
+            //return new OkObjectResult(_products);
+            return new OkObjectResult(JsonConvert.SerializeObject(_products));
+        }
+    }
+}
